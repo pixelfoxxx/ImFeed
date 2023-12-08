c4362181c09b46a30d18e9598c3d62f8b8b65c58
yurii kleimenov <yurakleimenovv@gmail.com>
2023-12-09 00:14:56 +0100

    Optimised navigation method for WebView

--- ImFeed/Auth/AuthViewController.swift
+++ ImFeed/Auth/AuthViewController.swift
@@ -12,12 +12,10 @@ class AuthViewController: UIViewController {
     //MARK: - Properties
     let ShowWebViewSegueIdentifier = "ShowWebView"
     
+    //MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == ShowWebViewSegueIdentifier {
-            guard let webViewViewController = segue.destination as? WebViewViewController
-            else {
-                fatalError("Failed to prepare for \(ShowWebViewSegueIdentifier)")
-            }
+        if segue.identifier == ShowWebViewSegueIdentifier,
+           let webViewViewController = segue.destination as? WebViewViewController {
             webViewViewController.delegate = self
         } else {
             super.prepare(for: segue, sender: sender)
