598a5c6e3085edb0e5a28d66bd90716cff7e3cf0
yurii kleimenov <yurakleimenovv@gmail.com>
2023-12-28 22:59:18 +0300

    Added ImagesListService for tests

---
+++ ImFeed/Models/Photo.swift
@@ -0,0 +1,30 @@
+//
+//  Photo.swift
+//  ImFeed
+//
+//  Created by Юрий Клеймёнов on 28/12/2023.
+//
+
+import Foundation
+
+struct Photo {
+    let id: String
+    let size: CGSize
+    let createdAt: Date?
+    let welcomeDescription: String?
+    let thumbImageURL: String
+    let largeImageURL: String
+    let isLiked: Bool
+}
+
+extension Photo {
+    init(from result: PhotoResult) {
+        self.id = result.id
+        self.size = CGSize(width: result.width, height: result.height)
+        self.createdAt = result.createdAt
+        self.welcomeDescription = result.description
+        self.thumbImageURL = result.urls.thumb
+        self.largeImageURL = result.urls.full
+        self.isLiked = result.likedByUser
+    }
+}
---
+++ ImFeed/Services/Images/ImagesListService.swift
@@ -0,0 +1,66 @@
+//
+//  ImagesListService.swift
+//  ImFeed
+//
+//  Created by Юрий Клеймёнов on 28/12/2023.
+//
+
+import Foundation
+
+final class ImagesListService {
+    // MARK: - Constants
+    static let shared = ImagesListService()
+    static let DidChangeNotification = Notification.Name("ImagesListServiceDidChange")
+    
+    private static let baseURLString = "https://api.unsplash.com/photos/"
+    
+    // MARK: - Properties
+    private let tokenStorage = OAuth2TokenStorage.shared
+    private (set) var photos: [Photo] = []
+    private var isFetching = false
+    private var currentPage = 0
+    
+    // MARK: - Public Methods
+    func fetchPhotosNextPage() {
+        guard !isFetching else { return }
+        
+        isFetching = true
+        currentPage += 1
+        guard let request = createRequest(page: currentPage) else {
+            isFetching = false
+            return
+        }
+        
+        let task = URLSession.shared.objectTask(for: request) { [weak self] (result: Result<[PhotoResult], Error>) in
+            DispatchQueue.main.async {
+                self?.isFetching = false
+                switch result {
+                case .success(let photoResults):
+                    self?.handleSuccess(photoResults: photoResults)
+                case .failure:
+                    self?.currentPage -= 1
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    // MARK: - Private Methods
+    private func createRequest(page: Int) -> URLRequest? {
+        guard let url = URL(string: Self.baseURLString + "?page=\(page)"),
+              let token = tokenStorage.token else {
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        return request
+    }
+    
+    private func handleSuccess(photoResults: [PhotoResult]) {
+        let newPhotos = photoResults.map { Photo(from: $0) }
+        photos.append(contentsOf: newPhotos)
+        NotificationCenter.default.post(name: Self.DidChangeNotification, object: self)
+    }
+}
