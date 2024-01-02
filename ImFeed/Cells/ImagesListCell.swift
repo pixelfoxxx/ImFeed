c8f0cbe418150a353f39ae0b3ec9b1d9b2801e91
yurii kleimenov <yurakleimenovv@gmail.com>
2024-01-02 21:31:42 +0300

    Complete cell configuration logic

--- ImFeed/Cells/ImagesListCell.swift
+++ ImFeed/Cells/ImagesListCell.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Kingfisher
 
 final class ImagesListCell: UITableViewCell {
     // MARK: - Cell Identifier
@@ -17,6 +18,32 @@ final class ImagesListCell: UITableViewCell {
     }()
     
     private var gradientLayer: CAGradientLayer?
+    
+    // MARK: - Lifecycle
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer?.frame = gradientView.bounds
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureGradientView()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        cellImageView.kf.cancelDownloadTask()
+    }
+    
+    // MARK: - Private Methods
+    func configure(with photo: Photo, dateFormatter: DateFormatter) {
+        cellImageView.kf.indicatorType = .activity
+        cellImageView.kf.setImage(with: URL(string: photo.smallImageURL), placeholder: UIImage(named: "loading_stub"))
+        
+        dateLabel.text = dateFormatter.string(from: Date())
+        let isLiked = photo.isLiked
+        let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
+        likeButton.setImage(likeImage, for: .normal)
+    }
     
     private func addGradient() {
         let gradientLayer = CAGradientLayer()
@@ -38,19 +65,9 @@ final class ImagesListCell: UITableViewCell {
             gradientView.trailingAnchor.constraint(equalTo: cellImageView.trailingAnchor),
             gradientView.leadingAnchor.constraint(equalTo: cellImageView.leadingAnchor),
             gradientView.bottomAnchor.constraint(equalTo: cellImageView.bottomAnchor)
-        
+            
         ])
         
         addGradient()
-    }
-
-    // MARK: - Lifecycle
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        gradientLayer?.frame = gradientView.bounds
-    }
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        configureGradientView()
     }
 }
--- ImFeed/ImagesListView/ImagesListViewController.swift
+++ ImFeed/ImagesListView/ImagesListViewController.swift
@@ -3,7 +3,7 @@ import Kingfisher
 
 final class ImagesListViewController: UIViewController {
     // MARK: - Properties
-    private let photosNames: [String] = (0..<20).map { "\($0)" }
+    private var photos: [Photo] = []
     private let ShowSingleImageSegueIdentifier = "ShowSingleImage"
     private let imagesListService = ImagesListService()
     
@@ -36,7 +36,14 @@ final class ImagesListViewController: UIViewController {
         }
     }
     
+    // MARK: - Cell Configuration
+    private func configureCell(_ cell: ImagesListCell, for indexPath: IndexPath) {
+        let photo = photos[indexPath.row]
+        cell.configure(with: photo, dateFormatter: dateFormatter)
+    }
+    
     // MARK: - Private Methods
+    
     private func configureTableView() {
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
         tableView.dataSource = self
@@ -52,15 +59,29 @@ final class ImagesListViewController: UIViewController {
                 queue: .main
             ) { [weak self] _ in
                 guard let self = self else { return }
-                self.tableView.reloadData()
+                updateTableViewAnimated()
             }
+    }
+    
+    private func updateTableViewAnimated() {
+        let oldCount = photos.count
+        let newCount = imagesListService.photos.count
+        photos = imagesListService.photos
+        if oldCount != newCount {
+            tableView.performBatchUpdates {
+                let indexPaths = (oldCount..<newCount).map { i in
+                    IndexPath(row: i, section: 0)
+                }
+                tableView.insertRows(at: indexPaths, with: .automatic)
+            } completion: { _ in }
+        }
     }
 }
 
 // MARK: - UITableViewDataSource
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return imagesListService.photos.count
+        return photos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -70,15 +91,16 @@ extension ImagesListViewController: UITableViewDataSource {
         configureCell(cell, for: indexPath)
         return cell
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard let image = UIImage(named: photosNames[indexPath.row]) else {
-            return 0
-        }
-        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
-        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
-        let imageWidth = image.size.width
-        let scale = imageViewWidth / imageWidth
-        return image.size.height * scale + imageInsets.top + imageInsets.bottom
+        let photo = photos[indexPath.row]
+        let imageHeight = photo.size.height
+        let imageWidth = photo.size.width
+        
+        let tableViewWidth = tableView.bounds.width
+        let scaleFactor = tableViewWidth / imageWidth
+        
+        return imageHeight * scaleFactor
     }
 }
 
@@ -87,22 +109,11 @@ extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: ShowSingleImageSegueIdentifier, sender: indexPath)
     }
-}
-
-// MARK: - Cell Configuration
-extension ImagesListViewController {
-    private func configureCell(_ cell: ImagesListCell, for indexPath: IndexPath) {
-        
-        if indexPath.row < imagesListService.photos.count {
-            let photo = imagesListService.photos[indexPath.row]
-            if let url = URL(string: photo.smallImageURL) {
-                cell.cellImageView.kf.setImage(with: url, placeholder: UIImage(named: "loading_stub"))
-            }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let lastElement = imagesListService.photos.count - 1
+        if indexPath.row == lastElement {
+            imagesListService.fetchPhotosNextPage()
         }
-        
-        cell.dateLabel.text = dateFormatter.string(from: Date())
-        let isLiked = indexPath.row % 2 == 0
-        let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
-        cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
