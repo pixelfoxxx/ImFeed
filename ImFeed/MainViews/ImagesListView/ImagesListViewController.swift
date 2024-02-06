2938b96489f58a112b2ada73fb7f7f8f51c0fa89
yurii kleimenov <yurakleimenovv@gmail.com>
2024-02-06 02:41:21 +0100

    ImagesListView refactoring completed

--- ImFeed/MainViews/ImagesListView/ImagesListViewController.swift
+++ ImFeed/MainViews/ImagesListView/ImagesListViewController.swift
@@ -1,13 +1,22 @@
+//
+//  ImagesListViewController.swift
+//  ImFeed
+//
+//  Created by Юрий Клеймёнов on 28/12/2023.
+//
+
 import UIKit
 import Kingfisher
 
-final class ImagesListViewController: UIViewController {
+// MARK: - ImagesListViewProtocol
+protocol ImagesListViewProtocol: AnyObject {
+    func updateTableViewAnimated()
+}
+
+// MARK: - ImagesListViewController
+final class ImagesListViewController: UIViewController, ImagesListViewProtocol {
     
     // MARK: - Properties
-    private var photos: [Photo] = []
-    private let imagesListService = ImagesListService()
-    
-    // MARK: - Date Formatter
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
@@ -22,13 +31,20 @@ final class ImagesListViewController: UIViewController {
         return tableView
     }()
     
+    var presenter: ImagesListPresenterProtocol!
+    private var photos: [Photo] = []
     
     // MARK: - Lifecycle
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        presenter.fetchPhotosNextPage()
+        presenter.addObserver()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter = ImagesListPresenter(view: self)
         configureTableView()
-        addImageListObserver()
-        imagesListService.fetchPhotosNextPage()
     }
     
     // MARK: - Cell Configuration
@@ -63,24 +79,94 @@ final class ImagesListViewController: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
+}
+// MARK: - UITableViewDataSource
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
     
-    private func addImageListObserver() {
-        NotificationCenter.default.addObserver(
-            forName: ImagesListService.didChangeNotification,
-            object: nil,
-            queue: .main) { [weak self] _ in
-                self?.updateTableViewAnimated()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        configureCell(cell, for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let photo = photos[indexPath.row]
+        let imageHeight = photo.size.height
+        let imageWidth = photo.size.width
+        
+        let tableViewWidth = tableView.bounds.width
+        let scaleFactor = tableViewWidth / imageWidth
+        
+        return imageHeight * scaleFactor
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let lastElement = ImagesListService.shared.photos.count - 1
+        if indexPath.row == lastElement {
+            presenter.fetchPhotosNextPage()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showSingleImageViewController(with: ImagesListService.shared.photos[indexPath.row])
+    }
+    
+    private func showSingleImageViewController(with photo: Photo) {
+        let singleImageVC = SingleImageViewController()
+        singleImageVC.photo = photo
+        let navigationController = UINavigationController(rootViewController: singleImageVC)
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true, completion: nil)
+    }
+}
+
+// MARK: - ImagesListCellDelegate
+extension ImagesListViewController: ImagesListCellDelegate {
+    func imageListCellDidTapLike(_ cell: ImagesListCell) {
+        guard let indexPath = tableView.indexPath(for: cell) else { return }
+        let photo = photos[indexPath.row]
+        let isLike = !photo.isLiked
+        
+        UIBlockingProgressHUD.show()
+        
+        ImagesListService.shared.changeLike(photoId: photo.id, isLike: isLike) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success():
+                    self?.photos[indexPath.row].isLiked = isLike
+                    self?.configureCell(cell, for: indexPath)
+                    UIBlockingProgressHUD.dismiss()
+                case .failure(let error):
+                    UIBlockingProgressHUD.dismiss()
+                    AlertPresenter.showConfirmationAlert(
+                        on: self!,
+                        title: "Ошибка: \(error)",
+                        message: "Что-то пошло не так. Попробовать ещё раз?",
+                        yesActionTitle: "Повторить",
+                        noActionTitle: "Не надо",
+                        yesAction: { [weak self] in
+                            self?.imageListCellDidTapLike(cell)
+                        })
+                }
             }
+        }
     }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self)
-    }
-    
-    private func updateTableViewAnimated() {
+}
+
+// MARK: - updateTableViewAnimated
+extension ImagesListViewController {
+    func updateTableViewAnimated() {
         let oldCount = photos.count
-        let newCount = imagesListService.photos.count
-        photos = imagesListService.photos
+        let newCount = ImagesListService.shared.photos.count
+        photos = ImagesListService.shared.photos
         
         tableView.performBatchUpdates {
             let indexPaths = (oldCount..<newCount).map { IndexPath(row: $0, section: 0) }
@@ -88,84 +174,3 @@ final class ImagesListViewController: UIViewController {
         }
     }
 }
-    // MARK: - UITableViewDataSource
-    extension ImagesListViewController: UITableViewDataSource {
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return photos.count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) as? ImagesListCell else {
-                return UITableViewCell()
-            }
-            configureCell(cell, for: indexPath)
-            return cell
-        }
-        
-        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            let photo = photos[indexPath.row]
-            let imageHeight = photo.size.height
-            let imageWidth = photo.size.width
-            
-            let tableViewWidth = tableView.bounds.width
-            let scaleFactor = tableViewWidth / imageWidth
-            
-            return imageHeight * scaleFactor
-        }
-    }
-    
-    // MARK: - UITableViewDelegate
-    extension ImagesListViewController: UITableViewDelegate {
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            showSingleImageViewController(with: photos[indexPath.row])
-        }
-        
-        private func showSingleImageViewController(with photo: Photo) {
-            let singleImageVC = SingleImageViewController()
-            singleImageVC.photo = photo
-            let navigationController = UINavigationController(rootViewController: singleImageVC)
-            navigationController.modalPresentationStyle = .fullScreen
-            present(navigationController, animated: true, completion: nil)
-        }
-        
-        func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-            let lastElement = imagesListService.photos.count - 1
-            if indexPath.row == lastElement {
-                imagesListService.fetchPhotosNextPage()
-            }
-        }
-    }
-    
-    // MARK: - ImagesListCellDelegate
-    extension ImagesListViewController: ImagesListCellDelegate {
-        func imageListCellDidTapLike(_ cell: ImagesListCell) {
-            guard let indexPath = tableView.indexPath(for: cell) else { return }
-            let photo = photos[indexPath.row]
-            let isLike = !photo.isLiked
-            
-            UIBlockingProgressHUD.show()
-            imagesListService.changeLike(photoId: photo.id, isLike: isLike) { [weak self] result in
-                DispatchQueue.main.async {
-                    switch result {
-                    case .success():
-                        self?.photos[indexPath.row].isLiked = isLike
-                        self?.tableView.reloadRows(at: [indexPath], with: .none)
-                        UIBlockingProgressHUD.dismiss()
-                    case .failure(let error):
-                        UIBlockingProgressHUD.dismiss()
-                        
-                        let alert = UIAlertController(title: "Ошибка: \(error)", message: "Что-то пошло не так. Попробовать ещё раз?", preferredStyle: .alert)
-                        
-                        alert.addAction(UIAlertAction(title: "Не надо", style: .cancel, handler: nil))
-                        
-                        alert.addAction(UIAlertAction(title: "Повторить", style: .default, handler: { [weak self] _ in
-                            guard let strongSelf = self else { return }
-                            strongSelf.imageListCellDidTapLike(cell)
-                        }))
-                        
-                        self?.present(alert, animated: true, completion: nil)
-                    }
-                }
-            }
-        }
-    }
---
+++ ImagesListTests/ImagesListTests.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListTests.swift
+//  ImagesListTests
+//
+//  Created by Юрий Клеймёнов on 06/02/2024.
+//
+
+import XCTest
+
+final class ImagesListTests: XCTestCase {
+
+}
